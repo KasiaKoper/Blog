ac872ddeee47cb216a813a67251e0ee091ceb30e
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-16 12:59:04 +0200

    post creation form added

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -1,14 +1,25 @@
 package com.example.blog.controller;
 
+import com.example.blog.PostRepository.PostRepository;
 import com.example.blog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/posts")
 @Controller
 public class PostController {
+
+    private PostRepository postRepository;
+
+    //alternatively @Autowired, however constructor is more recommended
+    public PostController(PostRepository postRepository){
+        this.postRepository=postRepository;
+    }
 
     @GetMapping("/add")
     public String addPost(ModelMap modelMap) {
@@ -16,7 +27,12 @@ public class PostController {
         return "posts/add";             //new directory ("posts") created in templates
     }
 
-
+    @PostMapping("")
+    public String createPost(@ModelAttribute Post post, ModelMap modelMap){
+        postRepository.save(post);
+        modelMap.put("post",post);
+        return "posts/show";
+    }
 
 
 
